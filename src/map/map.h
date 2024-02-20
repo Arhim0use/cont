ee51ac3fa60f38556f0e9b6f1be7eb0750399393
Arhim0use <chingisbek@gmail.com>
2024-02-20 18:08:09 +0300

    insert tree in map

---
+++ src/map/map.h
@@ -0,0 +1,170 @@
+#ifndef CPP2_S21_CONTAINERS_MAP_MAP_H_
+#define CPP2_S21_CONTAINERS_MAP_MAP_H_
+
+#include <limits.h>
+#include <math.h>
+#include <stddef.h>
+
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+
+#include "RB_tree.h"
+
+namespace s21 {
+template <class Key, class Value, class Compare = std::less<Key>,
+          class Allocator = std::allocator<std::pair<const Key, Value>>>
+class map : RB_tree<Key, Value> {
+ public:
+  using size_type = std::size_t;
+  using key_type = Key;
+  using mapped_type = Value;
+  using value_type = std::pair<const Key, Value>;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using difference_type = std::ptrdiff_t;
+  using iterator = typename RB_tree<Key, Value>::Iterator<false>;
+  using const_iterator = typename RB_tree<Key, Value>::Iterator<true>;
+  // struct Compare {
+  //   bool operator()(const_reference value1, const_reference value2) {
+  //     return value1.first < value2.first;
+  //   }
+  // };
+  using tree_type = RB_tree<key_type, mapped_type>;
+
+ public:
+  // Constructors
+  map() : tree_(new tree_type{}), size_(0) {}
+
+  // map(const map &other) : size_(other.size_) { tree_ = other.tree_; }
+
+  // map(map &&other) noexcept : tree_(new tree_type(std::move(*other.tree_)))
+  // {}
+
+  // // map(std::initializer_list<value_type>& items) {}
+
+  ~map() { delete tree_; }
+
+  // bool operator==(const map& other) {
+  //   return
+  // }
+
+  // // Assignment Operators
+  // map &operator=(const map &other) {
+  //   tree_ = other.tree_;
+  //   size_ = other.size_;
+  //   return *this;
+  // }
+
+  // map &operator=(map &&other) {
+  //   tree_ = std::move(other.tree_);
+  //   size_ = other.size_;
+  // }
+
+  // map &operator=(std::initializer_list<value_type> &items) {}
+
+  // Access Operators
+  // Value &at(const Key &key) {}
+
+  // Value &operator[](
+  //     const Key &key) { /* Insert if key doesn't exist, then return reference
+  //     */
+  // }
+
+  // const Value &at(const Key &key) const { /* Const version of at */
+  // }
+
+  // const Value &operator[](const Key &key) const { /* Const version of
+  // operator[]
+  // */
+  // }
+
+  // // Iterators
+  // iterator begin() { /* Begin iterator */
+  // }
+
+  // iterator end() { /* End iterator */
+  // }
+
+  // reverse_iterator rbegin() { return reverse_iterator(end()); }
+
+  // reverse_iterator rend() { return reverse_iterator(begin()); }
+
+  // const_iterator cbegin() const { /* Const begin iterator */
+  // }
+
+  // const_iterator cend() const { /* Const end iterator */
+  // }
+
+  // const_reverse_iterator crbegin() const {
+  //   return const_reverse_iterator(cend());
+  // }
+
+  // const_reverse_iterator crend() const {
+  //   return const_reverse_iterator(cbegin());
+  // }
+
+  // Capacity
+  bool empty() const { return size_ == 0; }
+
+  size_type size() const noexcept { return size_; }
+
+  size_type max_size() const noexcept {
+    return std::numeric_limits<difference_type>::max() / sizeof(RB_tree<Key, Value>);
+  }
+
+  //   size_type max_size() const noexcept { return
+  // std::numeric_limits<difference_type>::max(); }
+
+  // size_type max_size() const noexcept { return pow(2, 61) / 10; }
+  // size_type max_size() const noexcept { return size_; }
+
+  // size_type max_size() const noexcept { return
+  // std::numeric_limits<size_type>::max(); }
+
+  // Modifiers
+  // void clear();
+   /* std::pair<iterator, bool> */void insert(const value_type& value){
+    tree_->InsertUniq(value);
+   }
+   /* std::pair<iterator, bool> */ void insert(const Key& key, const Value& obj){
+    tree_->InsertUniq(std::make_pair(key, obj));
+
+   }
+  //  std::pair<iterator, bool> insert_or_assign(const Key& key, const Value&
+  //  obj); void erase(iterator pos);
+  // void swap(map &other);
+  // void merge(map &other);
+
+  // Lookup
+  // size_type count(const key_type &x) const {}
+
+  iterator find(const key_type &x) { return tree_->find(x);}
+
+  // const_iterator find(const key_type &x) const {}
+
+  // bool contains(const key_type &key) const {}
+
+  // // Bonus Part
+  // template <typename... Args>
+  // map<std::pair<iterator, bool>> insert_many(const iterator pos,
+  //                                            Args &&...args);
+
+  // temporary helper
+  void PrintMap() {
+    std::cout << "Printing map:" << std::endl;
+    for (const auto print : *this) {
+      std::cout << "key = " << (print.first) << ", value = " << (print.second) << std::endl;
+    }
+  }
+  void PrintMapRecursive(){
+    tree_->PrintTreeRecursive(tree_->RootIterartor(), 0);
+  }
+
+ private:
+  tree_type *tree_;
+  size_type size_;
+};
+}  // namespace s21
+
+#endif  // CPP2_S21_CONTAINERS_MAP_MAP_H_
---
+++ src/map/tree_node.tpp
@@ -0,0 +1,100 @@
+#pragma once
+#include "RB_tree.h"
+
+namespace s21 {
+// template<typename Key> // вариант №1
+// struct RB_tree<Key>::TreeNode_{
+template<typename Key, typename Value>// вариант №2
+struct RB_tree<Key, Value>::TreeNode_{
+  
+  // using value_type = Key; // вариант №1
+  using value_type = std::pair<Key, Value>; // вариант №2
+  using node_pointer = TreeNode_ *;
+    value_type data_;
+    node_pointer parent_;
+    node_pointer left_;
+    node_pointer right_;
+    // Red - 0 Black - 1
+    bool color_;
+
+    explicit TreeNode_() 
+              : data_(value_type()), parent_(nullptr), left_(nullptr), right_(nullptr), color_(RedN) {}
+
+    explicit TreeNode_(const TreeNode_& other) : data_(other.data_), parent_(other.parent_), 
+              left_(other.left_), right_(other.right_), color_(other.color_) {}
+
+    explicit TreeNode_(TreeNode_&& other) : data_(std::move(other.data_)), parent_(std::move(other.parent_)), 
+              left_(std::move(other.left_)), right_(std::move(other.right_)), color_(std::move(other.color_)) {}
+  // // вариант №2
+  // bool operator==(const std::pair<Key, Value>& other) const { return data_.first == other.first; }
+  // bool operator!=(const std::pair<Key, Value>& other) const { return data_.first != other.first; }
+  // bool operator<(const std::pair<Key, Value>& other) const { return data_.first < other.first; }
+  // bool operator>(const std::pair<Key, Value>& other) const { return data_.first > other.first; }
+  // bool operator<=(const std::pair<Key, Value>& other) const { return data_.first <= other.first; }
+  // bool operator>=(const std::pair<Key, Value>& other) const { return data_.first >= other.first; }
+/*
+Если у текущего узла есть правое поддерево,
+следующим узлом будет наименьший узел в 
+этом правом поддереве (т.е. самый левый узел в правом поддереве).
+Если у текущего узла нет правого поддерева, 
+следующим узлом будет ближайший предок, 
+для которого текущий узел находится в левом поддереве.
+*/
+  node_pointer NextNode() const noexcept {
+    node_pointer next = const_cast<node_pointer>(this);
+    if (!next->NextIsN_null(2)){
+      next = next->right_;
+      while(!next->NextIsN_null(1)) next = next->left_;
+    } else if (next->NextIsN_null(2) && !next->NextIsN_null(0)){
+      while (!next->NextIsN_null(0) && next != next->parent_->left_){
+        next = next->parent_;
+      }
+      next = next->parent_;
+    } 
+    else {
+      next = next->parent_;
+    }
+    return next;
+  }
+/*
+Если у текущего узла есть левое поддерево, предыдущим 
+узлом будет наибольший узел в этом левом поддереве 
+(т.е. самый правый узел в левом поддереве).
+Если у текущего узла нет левого поддерева, предыдущим 
+узлом будет ближайший предок, для которого текущий 
+узел находится в правом поддереве.
+*/
+  node_pointer PrevNode() const noexcept {
+    node_pointer prev = const_cast<node_pointer>(this);
+    if (!prev->NextIsN_null(1)){
+      prev = prev->left_;
+      while(!prev->NextIsN_null(2)) prev = prev->right_;
+    } else if (prev->NextIsN_null(1) && !prev->NextIsN_null(0)){
+      while(!prev->NextIsN_null(0) && prev != prev->parent_->right_){
+        prev = prev->parent_;
+      }
+      prev = prev->parent_;
+    } 
+    else {
+      prev = prev->parent_;
+    }
+    return prev;
+  }
+
+  void SetColor(bool new_color) noexcept { this->color_ = new_color; }
+  // изменить цвет определенной ноды
+  void SwitchNodeColor() noexcept { this->SetColor(!this->GetColor()); }
+  /// int next 0 - parent; 1 - left; 2 - right;
+  /// @return 0 - следущая нода не n_null; 1 - следущая нода n_null;
+  bool NextIsN_null(int next) noexcept {
+    if(!next){
+      return this->parent_ == this->parent_->right_;
+    } else if(next == 1){
+      return this->left_ == this->left_->right_;
+    } else if (next == 2){
+      return this->right_ == this->right_->right_;
+    }
+    return false;
+  }
+};
+} // namespace s21
